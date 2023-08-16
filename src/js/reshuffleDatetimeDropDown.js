5d0d024982e30d2b636dc8f37fa86c9f33016b90
Mario Parra <mario.parra@whoiam.ai>
2023-08-15 19:06:44 -0600

    rearrange readonly dateofbirth

--- src/js/reshuffleDatetimeDropDown.js
+++ src/js/reshuffleDatetimeDropDown.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
   // Store the select elements in variables
   var $daySelect = $('#dateOfBirth_day');
   var $monthSelect = $('#dateOfBirth_month');
-  var $yearSelect = $('#dateOfBirth_year');
 
   // Detach and re-append the elements in the desired order
   $monthSelect.detach().insertBefore($daySelect);
+
+    // Store the select elements in variables
+    $daySelect = $('#readonly_DateOfBirth_day');
+    $monthSelect = $('#readonly_DateOfBirth_month');
+
+    // Detach and re-append the elements in the desired order
+    $monthSelect.detach().insertBefore($daySelect);
 });
