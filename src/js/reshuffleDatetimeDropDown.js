1cadfe4dac4ad7b08358501f4e0faccc7b65fa06
Mario Parra <mario.parra@whoiam.ai>
2023-08-15 18:56:38 -0600

    Shuffle Datetiem drop down

---
+++ src/js/reshuffleDatetimeDropDown.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // Store the select elements in variables
+  var $daySelect = $('#dateOfBirth_day');
+  var $monthSelect = $('#dateOfBirth_month');
+  var $yearSelect = $('#dateOfBirth_year');
+
+  // Detach and re-append the elements in the desired order
+  $monthSelect.detach().insertBefore($daySelect);
+  $yearSelect.detach().insertBefore($monthSelect);
+});
